ca7290491b3b14f43e1fbbe92fc7e8ca515d5ae9
rajeevsheoran <rajeevshoran7@gmail.com>
2026-07-16 16:49:12 +0530

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        // bY Space Optimistion Method
+        int m =text1.length();
+        int n = text2.length();
+        vector<int> curr(n+1,0);
+        vector<int> prev(n+1,0);
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                // smehi
+                if(text1[i] == text2[j]){
+                    curr[j] = 1+ prev[j+1];
+                }
+                else{
+                    curr[j] = max(curr[j+1],prev[j]);
+                }
+            }
+            prev=curr;
+        }
+        return curr[0];
+    }
+};
